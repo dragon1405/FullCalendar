b4e410b31990f931fa04a60d426b704ce4789ef6
dragon1405 <54219825+dragon1405@users.noreply.github.com>
2020-08-11 21:02:25 -0500

    Update add_events.php

--- add_events.php
+++ add_events.php
@@ -1,21 +1,19 @@
 
 <?php
- 
+ //-----------Test
 $title=$_POST['title'];
 $start=$_POST['start'];
 $end=$_POST['end'];
  
-// connexion à la base de données
+// connexion Ã  la base de donnÃ©es
  try {
  $bdd = new PDO('mysql:host=localhost;dbname=fullcalendar', 'root', '');
  } catch(Exception $e) {
- exit('Impossible de se connecter à la base de données.');
+ exit('Impossible de se connecter Ã  la base de donnÃ©es.');
  }
  
 $sql = "INSERT INTO evenement (title, start, end) VALUES (:title, :start, :end)";
 $q = $bdd->prepare($sql);
 $q->execute(array(':title'=>$title, ':start'=>$start, ':end'=>$end));
 ?>
-
-
 
